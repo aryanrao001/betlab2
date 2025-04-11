6059f07a28d0a16c8fe25a4cc595dea00c96f797
aryanrao001 <portfoliorajatyadav@gmail.com>
2025-04-11 18:53:21 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 // import { Route, Router,Routes } from 'lucide-react'
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
